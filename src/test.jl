2169c258c4cd77eee819931b8bdbe330717fc2a2
Johannes Wesch <johannes.wesch@gmail.com>
2023-06-15 16:42:36 +0200

    tested jupyternotebook

--- src/test.jl
+++ src/test.jl
@@ -1,3 +1,6 @@
 using VNNLib
 
-network = load_network("benchmarks/mnistfc/mnist-net_256x4.onnx")
+f, n_input, n_output = get_ast("benchmarks/mnistfc/prop_0_0.03.vnnlib")
+# network = load_network("benchmarks/mnistfc/mnist-net_256x4.onnx")
+
+println(n_input)
