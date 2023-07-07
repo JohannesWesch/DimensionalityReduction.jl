d928b704c0ae6e9a34b9a3cf956bc9ce80beb518
Johannes Wesch <johannes.wesch@gmail.com>
2023-07-07 13:23:27 +0200

    tests

--- test/example.jl
+++ test/example.jl
@@ -3,4 +3,4 @@ import .DimensionalityReduction: reduce
 
 reduce("benchmarks/digits/digit-net_16x2.onnx", 
         "benchmarks/digits/dim16/prop_0_0.20.vnnlib",
-        "benchmarks/digits_reduced", method=1, d_to_reduce=0, nnenum=true, factorization=0)
+        "benchmarks/digits_reduced", method=1, d_to_reduce=-1, nnenum=true, factorization=2)
