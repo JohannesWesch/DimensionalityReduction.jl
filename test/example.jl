41dd7aa2ecad69204b852989dce342f20de9eb0e
Johannes Wesch <johannes.wesch@gmail.com>
2023-07-24 18:12:20 +0200

    deleted jupyter notebook

--- test/example.jl
+++ test/example.jl
@@ -18,7 +18,7 @@ import .DimensionalityReduction: reduce
 
 reduce("benchmarks/digits/digit-net_16x2.onnx", 
 "benchmarks/digits/dim16/prop_12_0.01.vnnlib",
-"benchmarks/digits_reduced", method=1, d_to_reduce=0, nnenum=true, factorization=1)
+"benchmarks/digits_reduced", method=1, d_to_reduce=11, nnenum=true, factorization=1)
 
 #5
 #"benchmarks/digits/dim16/prop_6_0.01.vnnlib"
